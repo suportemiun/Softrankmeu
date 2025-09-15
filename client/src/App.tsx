1ab787feeff72a15f95f68b324f3eea7e3539524
suportemiun <kluivertrapha@gmail.com>
2025-09-15 19:29:34 +0000

    Restored to 'a33a54417e5ff607cbfabee904083e26ca3f5b74'
    Replit-Restored-To: a33a54417e5ff607cbfabee904083e26ca3f5b74

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,7 +3,6 @@ import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
-import CursorEffect from "@/components/CursorEffect";
 import Home from "@/pages/Home";
 import Order from "@/pages/Order";
 import NotFound from "@/pages/not-found";
@@ -23,7 +22,6 @@ function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <TooltipProvider>
-        <CursorEffect />
         <Toaster />
         <Router />
       </TooltipProvider>
